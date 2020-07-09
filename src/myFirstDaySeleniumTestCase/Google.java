016507c3214b1c390d9ccbabb31f5ba62cb220b9
akanksha <akankshamajjiga@gmail.com>
2020-07-09 19:52:52 +0530

    first commit

---
+++ src/myFirstDaySeleniumTestCase/Google.java
@@ -0,0 +1,42 @@
+package myFirstDaySeleniumTestCase;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Google {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.setProperty("webdriver.chrome.driver", "D:\\akanksha\\chromedriver_win32\\chromedriver.exe");
+		
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://www.google.co.in/");
+		
+		String title = driver.getTitle();
+		
+		System.out.println(title);
+		
+		//validation point
+		
+		if(title.equals("Google")) {
+			
+			System.out.println("correct");
+			
+		}
+		
+		else {
+			
+			System.out.println("in-correct");
+			
+		}
+		
+		System.out.println(driver.getCurrentUrl());
+		
+	   //System.out.println(driver.getPageSource());
+		
+		driver.quit();
+	}
+
+}
